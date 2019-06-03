52c91880045b3889ebb0d01fa35f3ead31cda34d
venetrius <gergelygjuhasz@gmail.com>
2019-06-03 17:09:27 -0600

    add 'MessageList' component

---
+++ src/MessageList.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import Message from './Message.jsx';
+
+class MessageList extends Component {
+  
+  constructor(props){
+    super();
+    this.messageList = props.messageList;
+  }
+
+  render() {
+    const messageList = this.messageList.map((message, index) => <Message props={message} key={index}></Message> );
+    return (
+      <main key="messageList" className="messages">
+        {messageList}
+      </main>
+      
+    );
+  }
+}
+export default MessageList;
