ee07325e8649e7234a6cf5e8b2d94d01f7ab3bdf
venetrius <gergelygjuhasz@gmail.com>
2019-06-03 17:09:14 -0600

    add 'Message' component

---
+++ src/Message.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function  Message({props, index}) {
+  let content;
+  console.log('props',props);
+  if(props.type === 'incomingMessage'){
+    content =  
+      <div className="message">
+        <span className="message-username">{props.username}</span>
+        <span className="message-content">{props.content}</span>
+      </div>
+  }else{
+    content = 
+      <div className="message system">
+         {props.content}
+     </div>
+  }
+  return (
+   <div>{content}</div>
+    
+    
+  );
+}
+export default Message;
