878ad9f7cb0d2be41fff49bb62fa278d7dfa25e5
venetrius <gergelygjuhasz@gmail.com>
2019-06-03 17:10:52 -0600

    use 'ChatBar' 'Message' and 'MessageList' components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
 import React, {Component} from 'react';
+import MessageList from './MessageList.jsx';
+import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      messages : [
+        {
+          type: 'incomingMessage',
+          content: 'I won\'t be impressed with technology until I can download food.',
+          username: 'Anonymous1'
+        },
+        {
+          type: 'incomingNotification',
+          content: 'Anonymous1 changed their name to nomnom',
+        } ]
+      }
+  }
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+        <MessageList messageList={this.state.messages}/>
+        <ChatBar/>
+      </div>
     );
   }
 }
 export default App;
+
+
+/*
+
+*/
