c0452f2069283da2d3b19f25b4af833dedd781ed
venetrius <gergelygjuhasz@gmail.com>
2019-06-05 14:28:11 -0600

    add updateUserName handler

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,19 @@ class App extends Component {
       messages : []
     };
     this.createNewMessage = this.createNewMessage.bind(this);
+    this.updateUserName = this.updateUserName.bind(this);
     this.connection = null;
   }
 
   componentDidMount() {
     this.connect();
+  }
+
+  updateUserName(newName){
+    const oldName = this.state.username;
+    if(newName && newName !== oldName){
+      this.setState({username : newName});
+    }
   }
 
   createNewMessage(username, content){
@@ -39,7 +47,7 @@ class App extends Component {
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
         <MessageList messageList={this.state.messages}/>
-        <ChatBar username={this.state.username} onNewMessage={this.createNewMessage}/>
+        <ChatBar username={this.state.username} updateUserName={this.updateUserName} onNewMessage={this.createNewMessage}/>
       </div>
     );
   }
