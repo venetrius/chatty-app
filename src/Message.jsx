67f8688604b6888f0877c176eb53e842bc38b74f
venetrius <gergelygjuhasz@gmail.com>
2019-06-05 18:13:01 -0600

    Display images in chat

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 
-function  Message({props, index}) {
+const urlRegExp = /((https:\/\/|http:\/\/)|((https:\/\/|http:\/\/)?www\.)|www\.)..([^ \t\r\n\v\f])*\.([^ \t\r\n\v\f])*\.(jpg|png|gif)/g
+
+const machImgUrl= function (content){
+  const res = urlRegExp.exec(content);
+  return res ? res[0] : res;
+}
+
+const createPost = function(content){
+  const imgUrl = machImgUrl(content);
+  const img = imgUrl ? <img src={imgUrl} /> : '';
+  const text = imgUrl ? content.replace(imgUrl, '') : content;
+  return {img, text};
+}
+
+function  Message({props}) {
   let content;
   if(props.type === 'incomingMessage'){
+    const {img, text} = createPost(props.content);
     content =  
       <div className="message">
         <span className="message-username" style={{color : props.userColor}}>{props.username}</span>
-        <span className="message-content">{props.content}</span>
+        <span className="message-content">{text} <br/> {img}</span>
+        
+        
       </div>
   }else{
     content = 
