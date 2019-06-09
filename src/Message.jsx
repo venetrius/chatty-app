39481d850b8a48131ca7d6e7cf71fa7f056d7c6e
venetrius <gergelygjuhasz@gmail.com>
2019-06-09 16:31:13 -0600

    implement scroll down on message load

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 const urlRegExp = /((https:\/\/|http:\/\/)|((https:\/\/|http:\/\/)?www\.)|www\.)..([^ \t\r\n\v\f])*\.([^ \t\r\n\v\f])*\.(jpg|png|gif)/g
 
@@ -7,13 +7,13 @@ const machImgUrl= function (content){
   return res ? res[0] : res;
 }
 
-const createPost = function(content){
+const createPost = function(content, userColor, username){
   const imgUrl = machImgUrl(content);
   const img = imgUrl ? <img src={imgUrl} /> : '';
   const text = imgUrl ? content.replace(imgUrl, '') : content;
   return (      
     <div className="message">
-      <span className="message-username" style={{color : props.userColor}}>{props.username}</span>
+      <span className="message-username" style={{color : userColor}}>{username}</span>
       <span className="message-content">{text} <br/> {img}</span>        
     </div>
   );
@@ -23,10 +23,26 @@ const isMessage = function (props){
   return props.type === 'incomingMessage';
 }
 
-function  Message({props}) {
-  let content = isMessage(props) ? createPost(props.content) : (<div className="message system"> {props.content} </div>);
-  return (
-   <div>{content}</div>
-  );
+
+
+class  Message extends Component{
+  constructor(props) {
+    super(props);
+    this.container = React.createRef();
+  }
+
+  // scrolling down on first load
+  componentDidMount(){
+    this.container.current.scrollIntoView({ behavior: "smooth" });
+  }
+
+  render(){
+    let props = this.props.props; 
+    let content = isMessage(props) ? createPost(props.content, props.userColor, props.username) 
+                  : (<div className="message system"> {props.content} </div>);
+    return (
+    <div ref={this.container}>{content}</div>
+    );
+  }
 }
 export default Message;
